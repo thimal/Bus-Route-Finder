267c957b5dad7590645df91cec72f41c623842be
Thimal J <thimal@gmail.com>
2011-07-26 17:58:11 +0530

    added functionality to track weights for a route

--- source/htdocs/route.php
+++ source/htdocs/route.php
@@ -7,6 +7,11 @@
 
 	Refactoring and some features: TJ, 7/22/2011
 	See license.txt for license
+
+	Notes: 
+	7/26/2011: to maintain compatibility, hop count is considered equivalent to the path cost.
+	However, these two concepts are explicitly stored separately so weight can be influenced
+	by other factors and signals at a later date.
 	
 ******************************************************************************************/
 
@@ -16,6 +21,7 @@ class Route {
 	private $waypoints = array();
 	private $waypoint_order = array();
 	private $bus_order = array();
+	private $waypoint_weight = array(); // at least initially, assume weight = path cost in hops
 	
 	public function __construct() {
 	}
@@ -30,12 +36,13 @@ class Route {
 
 	public function push_waypoint($place_id, $bus_id, $cost) {
 		$this->waypoints[$place_id] = $cost;		
+		$this->waypoint_weight[$place_id] = $cost;
 		array_unshift($this->waypoint_order, $place_id);
-		array_unshift($this->bus_order, $bus_id);
+		array_unshift($this->bus_order, $bus_id);		
 	}
 
 	public function get_total_cost() {
-		return array_sum(array_values($this->waypoints));
+		return array_sum(array_values($this->waypoint_weight));
 	}
 
 	public function get_waypoints() {
